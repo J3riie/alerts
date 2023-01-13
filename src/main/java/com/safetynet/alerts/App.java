d2a92a3b72fea88644f0e3bf83bd8cf5cc8bec41
Robin Hugues <robin.hugues1@gmail.com>
2023-01-13 20:45:19 +0100

    first commit with first resource

--- src/main/java/com/safetynet/alerts/App.java
+++ src/main/java/com/safetynet/alerts/App.java
@@ -1,13 +1,32 @@
 package com.safetynet.alerts;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.util.Arrays;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class App {
+    private static final Logger logger = LogManager.getLogger(App.class);
+
+    public static void main(String[] args) {
+        SpringApplication.run(App.class, args);
+    }
+
+    @Bean
+    CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+        return args -> {
+            logger.info("Let's inspect the beans provided by Spring Boot:");
+            final String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (final String beanName : beanNames) {
+                logger.info(beanName);
+            }
+        };
     }
 }
---
+++ src/main/java/com/safetynet/alerts/dto/PersonDTO.java
@@ -0,0 +1,44 @@
+package com.safetynet.alerts.dto;
+
+public class PersonDTO {
+    private String firstName;
+
+    private String lastName;
+
+    private String address;
+
+    private String phone;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+}
